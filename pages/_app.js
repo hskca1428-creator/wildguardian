24ee25f5029ac04c0867076355aa9aef0e76a2f6
hskca1428-creator <hskca1428@gmail.com>
2025-10-05 16:22:59 +1100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,14 @@ import '../styles/globals.css'
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+import '../styles/globals.css'
+import Script from 'next/script'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Script src="https://js.stripe.com/v3/" strategy="beforeInteractive" />
+      <Component {...pageProps} />
+    </>
+  )
+}
